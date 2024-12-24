98284e86f2b0511008b879aa1550c1ca9520449b
kshitij1439 <kshitijgaikwad142004@gmail.com>
2024-12-24 10:15:59 +0530

    doing

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WeatherApp from './WeatherApp';
+function App() {
+
+  return (
+    <>
+      {/* <button>Hii</button>
+      <Button variant="contained">Click Me!!</Button> */}
+      <WeatherApp/>
+    </>
+  )
+}
+
+export default App
